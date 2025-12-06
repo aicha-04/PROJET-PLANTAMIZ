7cf4166fdf13041861e11de3897c75e524d1304e
MatthieuRa <matthieu.ratsimbaharijao@gmail.com>
2025-12-06 18:36:58 +0100

    Update fonction.h
    QUOTAS

--- PP/fonction.h
+++ PP/fonction.h
@@ -2,14 +2,14 @@
 
 // === TABLE DES COULEURS CONSOLE ===
 // 0 : Noir
-// 1 : Bleu foncé
-// 2 : Vert foncé
+// 1 : Bleu foncÃ©
+// 2 : Vert foncÃ©
 // 3 : Turquoise
-// 4 : Rouge foncé
+// 4 : Rouge foncÃ©
 // 5 : Violet
 // 6 : Vert caca d'oie
 // 7 : Gris clair
-// 8 : Gris foncé
+// 8 : Gris foncÃ©
 // 9 : Bleu fluo
 // 10 : Vert fluo
 // 11 : Turquoise
@@ -22,8 +22,10 @@ void gotoligcol(int lig,int col);
 void afficherTableau(char tab[LIG][COL], int curseurL, int curseurC);
 void afficherCase(char tab[LIG][COL], int lig,int col,int surligner,int selection);
 void appliquerGravite(char tab[LIG][COL]);
-void analyserEtEliminer(char tab[LIG][COL], int *score);
+void analyserEtEliminer(char tab[LIG][COL], int *score, int compteurElim[5], int quotas[5], int *finPartie);
 void verifierFinPartie(int *compteurMouvements, int maxCoups, int *finPartie);
 int sauvegarderPartie(char tab[LIG][COL], int maxCoups, int compteurMouvements, int curseurL, int curseurC, int score);
 int chargerPartie(char tab[LIG][COL], int *compteurMouvements, int *maxCoups, int *curseurL, int *curseurC, int *score);
-void boucleDeplacement(char tab[LIG][COL], int *curseurL, int *curseurC,int *compteurMouvements, int maxCoups, int *score, int *finPartie);
+void boucleDeplacement(char tab[LIG][COL], int *curseurL, int *curseurC,
+                       int *compteurMouvements, int maxCoups, int *score,
+                       int *finPartie, int compteurElim[5], int quotas[5]);

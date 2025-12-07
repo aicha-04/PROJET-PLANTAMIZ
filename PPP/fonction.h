bdc5a704750af0a310ac3be096b9c440eeee5564
MatthieuRa <matthieu.ratsimbaharijao@gmail.com>
2025-12-07 21:39:22 +0100

    Update fonction.h
    semi prime

--- PPP/fonction.h
+++ PPP/fonction.h
@@ -2,21 +2,22 @@
 
 // === TABLE DES COULEURS CONSOLE ===
 // 0 : Noir
-// 1 : Bleu foncé
-// 2 : Vert foncé
+// 1 : Bleu foncÃ©
+// 2 : Vert foncÃ©
 // 3 : Turquoise
-// 4 : Rouge foncé
+// 4 : Rouge foncÃ©
 // 5 : Violet
 // 6 : Vert caca d'oie
 // 7 : Gris clair
-// 8 : Gris foncé
+// 8 : Gris foncÃ©
 // 9 : Bleu fluo
 // 10 : Vert fluo
 // 11 : Turquoise
 // 12 : Rouge fluo
 // 13 : Violet 2
 // 14 : Jaune
-// 15 : Blanc// Déclaration du type Level avant son utilisation
+// 15 : Blanc// DÃ©claration du type Level avant son utilisation
+
 
 typedef struct {
     int quotas[5];
@@ -24,22 +25,32 @@ typedef struct {
     int tempsLimite;
 } Level;
 
-// === PROTOTYPES ===
+// Fonctions utilitaires
 void Color(int couleurDuTexte,int couleurDeFond);
-void gotoligcol(int lig,int col);
-void afficherTableau(char tab[LIG][COL], int curseurL, int curseurC);
-void verifierTimer(time_t debutPartie, int tempsLimite, int *finPartie);
+void gotoligcol(int lig, int col);
 
-void afficherCase(char tab[LIG][COL], int lig,int col,int surligner,int selection);
-void appliquerGravite(char tab[LIG][COL]);
-void analyserEtEliminer(char tab[LIG][COL], int *score, int compteurElim[5], int quotas[5], int *finPartie);
+// Affichage
+void afficherTableau(char tab[LIG][COL], int curseurL, int curseurC, char nomJoueur[]);
+void afficherCase(char tab[LIG][COL], int lig, int col, int surligner, int selection);
+
+// Jeu
 void verifierFinPartie(int *compteurMouvements, int maxCoups, int *finPartie);
+void appliquerGravite(char tab[LIG][COL]);
+void mettreAJourCompteurElimination(char tab[LIG][COL], int aEliminer[LIG][COL], int compteurElim[5], int quotas[5], char nomJoueur[]);
+int verifierVictoire(int compteurElim[5], int quotas[5]);
+void verifierTimer(time_t debutPartie, int tempsLimite, int *finPartie);
+void analyserEtEliminer(char tab[LIG][COL], int *score, int compteurElim[5], int quotas[5], int *finPartie, char nomJoueur[]);
+
+// Sauvegarde
 int sauvegarderPartie(char tab[LIG][COL], int niveauActuel, int maxCoups, int compteurMouvements,
-                      int curseurL, int curseurC, int score, int compteurElim[5], time_t debutPartie, int tempsLimite);
+                      int curseurL, int curseurC, int score, int compteurElim[5],
+                      time_t debutPartie, int tempsLimite, char nomJoueur[]);
 int chargerPartie(char tab[LIG][COL], int *niveauActuel, int *compteurMouvements, int *maxCoups,
                   int *curseurL, int *curseurC, int *score, int compteurElim[5],
-                  time_t *debutPartie, int *tempsLimite, int quotas[5], Level niveaux[]);
+                  time_t *debutPartie, int *tempsLimite, int quotas[5], Level niveaux[], char nomJoueur[]);
+
+// DÃ©placement
 void boucleDeplacement(char tab[LIG][COL], int *curseurL, int *curseurC,
                        int *compteurMouvements, int maxCoups, int *score,
                        int *finPartie, int compteurElim[5], int quotas[5],
-                       time_t debutPartie, int tempsLimite, int niveauActuel);
+                       time_t debutPartie, int tempsLimite, int niveauActuel, char nomJoueur[]);

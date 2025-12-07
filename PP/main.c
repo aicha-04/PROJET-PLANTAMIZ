b11f608b9fb134f5402ede953788519088f75412
MatthieuRa <matthieu.ratsimbaharijao@gmail.com>
2025-12-07 15:34:58 +0100

    Update main.c
    Ajout du système de niveau

--- PP/main.c
+++ PP/main.c
@@ -1,9 +1,6 @@
 #include "fonction.h"
 
-
-
 //======== MAIN ================================================================================================================
-#include "fonction.h"
 
 int main() {
     srand(time(NULL));
@@ -11,33 +8,53 @@ int main() {
     char tab[LIG][COL] = {0};
     int choix;
     int compteurMouvements = 0;
-    int maxCoups = 10;
+    int maxCoups = 0;
     int score = 0;
     int finPartie = 0;
     int curseurL = 0, curseurC = 0;
-    int compteurElim[5] = {0};       // Total de chaque type éliminé : S, F, P, O, M
-    int quotas[5] = {50, 30, 20, 0, 0}; // Quotas de victoire pour chaque symbole (modifiable)
-    int tempsLimite = 60;   // Durée de la partie en secondes (modifiable)
+    int compteurElim[5] = {0};
+    int quotas[5] = {0, 0, 0, 0, 0};
+    int tempsLimite = 0;
     time_t debutPartie = time(NULL);
 
+    // ================== SYSTEME DE NIVEAUX ===================
+    Level niveaux[] = {
+        { {20, 50, 20, 0, 0}, 30, 300 },   // Niveau 1
+        { {30, 40, 25, 5, 0}, 25, 250 },   // Niveau 2
+        { {40, 20, 30, 10, 0}, 20, 200 }   // Niveau 3
+    };
 
-    printf("Choisissez le tableau :\n");
-    printf("1 - Tableau 1\n");
+    int nbNiveaux = sizeof(niveaux) / sizeof(niveaux[0]);
+    int niveauActuel = 0; // On commence au niveau 1
+    // ==========================================================
+
+
+    printf("Que voulez vous faire :\n");
+    printf("1 - Commencer une nouvelle Partie \n");
     printf("2 - Charger la partie precedente\n");
     scanf("%d", &choix);
     system("cls");
 
-    if(choix == 1) {
+    if (choix == 1) {
         afficherTableau(tab, curseurL, curseurC);
-        printf("Tableau 1 sélectionné.\n");
+
+        // Charger les paramètres du niveau
+        for (int i = 0; i < 5; i++)
+            quotas[i] = niveaux[niveauActuel].quotas[i];
+
+        maxCoups = niveaux[niveauActuel].maxCoups;
+        tempsLimite = niveaux[niveauActuel].tempsLimite;
+
+        gotoligcol(LIG+1, 0);
+        printf("Niveau %d sélectionné.\n", niveauActuel + 1);
     }
-    else if(choix == 2) {
-        if(!chargerPartie(tab, &compteurMouvements, &maxCoups, &curseurL, &curseurC, &score)) {
+
+    else if (choix == 2) {
+        if (!chargerPartie(tab, &compteurMouvements, &maxCoups, &curseurL, &curseurC, &score)) {
             printf("Impossible de charger la sauvegarde.\n");
             return 0;
         }
 
-        // ➜ afficherTableau ne régénèrera pas le plateau si tab est déjà rempli
         afficherTableau(tab, curseurL, curseurC);
 
         Color(11,0);
@@ -52,9 +69,27 @@ int main() {
     gotoligcol(LIG+3, 0);
     printf("ZQSD = se déplacer | ESPACE = échanger | A = sauvegarder | E = quitter");
 
+    // ================== BOUCLE DE JEU ===================
     boucleDeplacement(tab, &curseurL, &curseurC, &compteurMouvements,
-                  maxCoups, &score, &finPartie, compteurElim, quotas,
-                  debutPartie, tempsLimite);
+                      maxCoups, &score, &finPartie, compteurElim, quotas,
+                      debutPartie, tempsLimite);
+
+    // Après la partie
+    if (finPartie == 1) { // Victoire
+        printf("\nBravo ! Vous avez terminé le niveau %d !\n", niveauActuel + 1);
+
+        if (niveauActuel < nbNiveaux - 1) {
+            niveauActuel++;
+            printf("Passage au niveau %d !\n", niveauActuel + 1);
+        }
+        else {
+            printf("Vous avez complété **TOUS** les niveaux !\n");
+        }
+    }
+    else if (finPartie == 2) {
+        printf("\nDommage, vous avez perdu.\n");
+    }
+    // =====================================================
 
     gotoligcol(LIG+3, 0);
     return 0;
